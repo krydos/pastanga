cf672b20983a74252757329945221dadc7a57db6
Ruslan Bekenev <furyinbox@gmail.com>
2013-12-23 12:32:55 +0200

    little fix for error page

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -3,13 +3,12 @@
 /* @var $error array */
 
 $this->pageTitle=Yii::app()->name . ' - Error';
-$this->breadcrumbs=array(
-	'Error',
-);
 ?>
 
-<h2>Error <?php echo $code; ?></h2>
-
-<div class="error">
-<?php echo CHtml::encode($message); ?>
+<div style="color:white;margin-left:212px;">
+    <h2>Error <?php echo $code; ?></h2>
+    
+    <div class="error">
+        <?php echo CHtml::encode($message); ?>
+    </div>
 </div>
