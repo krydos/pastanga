a50783c5ee29d0d50f0e6d46b803b97bfec47016
KryDos <furyinbox@gmail.com>
2014-03-23 21:03:06 +0200

    change html select with Ember.Select

--- js/ember_app/pastanga.js
+++ js/ember_app/pastanga.js
@@ -1,9 +1,26 @@
 Pastanga = Ember.Application.create();
 
 Pastanga.IndexController = Ember.Controller.extend({
+    syntaxList: [
+        {text: "Plain Text", value: 'plain'},
+        {text: "C", value: 'C'},
+        {text: "C++", value: 'CPP'},
+        {text: "CSS", value: 'css'},
+        {text: "Erlang", value: 'erlang'},
+        {text: "Haskell", value: 'hs'},
+        {text: "JavaScript", value: 'lua'},
+        {text: "Lisp", value: 'lisp'},
+        {text: "Pascal", value: 'pascal'},
+        {text: "Perl", value: 'PERL'},
+        {text: "PHP", value: 'PHP'},
+        {text: "Scala", value: 'scala'},
+        {text: "Shell", value: 'sh'},
+        {text: "SQL", value: 'sql'},
+        {text: "YAML", value: 'yaml'},
+    ],
     actions: {
         sendText: function() {
-            alert(this.get('pasteText'));
+            alert(this.get('syntax'));
         }
     }
 });
