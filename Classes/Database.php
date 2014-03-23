8f597c1f2c2ffbc4ac86efd54f3fee12a275e98f
KryDos <furyinbox@gmail.com>
2014-03-23 22:21:27 +0200

    oops. forgot about Helpers and Database classes

---
+++ Classes/Database.php
@@ -0,0 +1,31 @@
+<?php
+namespace Classes;
+
+class Database extends \PDO {
+
+    function __construct() {
+        parent::__construct('mysql:dbname=' . \Classes\Config::getDbName() . ';host=' . \Classes\Config::getHost(), \Classes\Config::getDbUsername(), \Classes\Config::getDbPassword());
+    }
+
+    public function saveText($text, $syntax, $link) {
+        $request = $this->prepare('INSERT INTO pastetext (text, link, type) VALUES (:text, :link, :syntax)');
+        $request->bindParam(':text', $text);
+        $request->bindParam(':link', $link);
+        $request->bindParam(':syntax', $syntax);
+
+        return $request->execute();
+    }
+
+    public function linkExist($link) {
+        $request = $this->prepare('SELECT link FROM pastetext WHERE link=:link'); 
+        $request->bindParam(':link', $link);
+        $request->execute();
+
+        if($request->rowCount() == 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
---
+++ Classes/Helpers.php
@@ -0,0 +1,42 @@
+<?php
+namespace Classes;
+
+class Helpers {
+
+    /**
+     * generate unique link
+     */
+    public static function generateLink($length = 4) {
+       while (true) {
+            $link = self::generateRandomString($length);
+
+            /* check for exist this link */
+            $db = new \Classes\Database();
+            if(!$db->linkExist($link))
+                return $link;
+            else
+                self::generateLink($length+1);
+        }
+    }
+
+    /**
+     * generate random string
+     * needed for generation random string with length $length
+     * using for link generation
+     *
+     * @param integer $length length of the string
+     *
+     * @return random string
+     */
+    protected static function generateRandomString($length)
+    {
+        $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+        $pass = array(); //remember to declare $pass as an array
+        $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+        for ($i = 0; $i < $length; $i++) {
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+        return implode($pass); //turn the array into a string
+    }
+}
