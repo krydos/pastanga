0722000d8094dfa68176773d9959c448c57e786e
KryDos <furyinbox@gmail.com>
2014-03-23 20:48:43 +0200

    add some actions

--- js/ember_app/pastanga.js
+++ js/ember_app/pastanga.js
@@ -1,1 +1,9 @@
 Pastanga = Ember.Application.create();
+
+Pastanga.IndexController = Ember.Controller.extend({
+    actions: {
+        sendText: function() {
+            alert(this.get('pasteText'));
+        }
+    }
+});
